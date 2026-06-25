351c5dde49205f0e2d4e393ed4a78f467fd2730e
Madalina <stoicovmadalina@gmail.com>
2026-06-24 19:00:30 -0700

    init

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+import yaml
+
+
+def load_config(path):
+    with open(path) as f:
+        return yaml.load(f.read(), Loader=yaml.Loader)
+
+
+def fetch(url):
+    return requests.get(url).json()
+
+
+if __name__ == "__main__":
+    print(load_config("config.yml"))
